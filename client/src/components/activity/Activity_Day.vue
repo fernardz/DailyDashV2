dc28d27c5bfe2efc2c424160c94603642ebbf485
Fernando Rodriguez <49568539+frodrig3ND@users.noreply.github.com>
2021-08-28 14:46:01 -0400

    activity dashboad

---
+++ client/src/components/activity/Activity_Day.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-sm-6">
+    <b-icon-watch></b-icon-watch>{{ actSum.time}} min
+    </div>
+    <div class="col-sm-6">
+    <b-icon-compass></b-icon-compass> {{ actSum.distance }} miles
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ActDaily',
+  computed: {
+    actSum() {
+      return this.$store.state.act_sum;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('getActSummary');
+  },
+};
+</script>
